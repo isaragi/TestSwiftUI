e80a78419f035cf79e2d515038d93c50d548b991
isaragi <isaragi1987@gmail.com>
2019-07-28 15:42:17 +0900

    tutorial2-3

--- TestSwiftUI/LandmarkRow.swift
+++ TestSwiftUI/LandmarkRow.swift
@@ -12,7 +12,10 @@ struct LandmarkRow: View {
     var landmark: Landmark
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            landmark.image(forSize: 50)
+            Text(landmark.name)
+        }
     }
 }
 
