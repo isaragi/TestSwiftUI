8e37c7a896e141355c4fab37230ec45fd284962e
isaragi <isaragi1987@gmail.com>
2019-07-28 15:22:42 +0900

    tutorial2-1

--- TestSwiftUI/LandmarkDetail.swift
+++ TestSwiftUI/LandmarkDetail.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LandmarkDetail.swift
 //  TestSwiftUI
 //
 //  Created by isaragi on 2019/07/27.
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
     var body: some View {
         VStack {
             MapView()
@@ -41,7 +41,7 @@ struct ContentView: View {
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail()
     }
 }
 #endif
